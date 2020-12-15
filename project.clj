2892490bd5baeb980020cb7f2f34eade6b331c0d
atsuo yamada <yamada-git2@tir.jp>
2020-12-16 01:52:11 +0900

    Released 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jp.ne.tir/tir-js-property "1.0.0"
+(defproject jp.ne.tir/tir-js-property "1.0.0-ADVANCED"
   :description "js property manipulator utility"
   :url "https://github.com/ayamada/tir-js-property"
   :license {:name "Zlib License"
